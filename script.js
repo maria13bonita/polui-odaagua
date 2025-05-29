be9b27c12106f6cca69e81c58e4ae482fe2543a5
Maria RIta <136738217+maria13bonita@users.noreply.github.com>
2025-05-29 14:14:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function aumentarFonte() {
+  document.body.style.fontSize = "1.2em";
+}
+
+function diminuirFonte() {
+  document.body.style.fontSize = "0.9em";
+}
+
+function altoContraste() {
+  document.body.classList.toggle("alto-contraste");
+}
